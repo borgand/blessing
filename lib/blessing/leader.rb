14d7336820f4ff95ebd103bcde4a1ff09cdf475c
Laas Toom <laas.toom@gmail.com>
2011-10-17 16:28:16 +0300

    added logging

--- lib/blessing/leader.rb
+++ lib/blessing/leader.rb
@@ -22,7 +22,10 @@ module Blessing
       @options = DefaultOptions.merge(opts)
       initialize_logger
 
+      logger.info "Starting Blessing::Leader"
       @patterns = patterns.is_a?(Array) ? patterns : [patterns]
+      logger.debug "Patterns: #{patterns.inspect}"
+
       @old_confdig_files = @config_files = []
       @runners = {}
     end
@@ -33,12 +36,13 @@ module Blessing
     end
 
     def daemonize!
-      logger.debug
+      logger.info "Daemonizing..."
       Daemons.daemonize
     end
 
     # Start running cycles
     def start
+      logger.info "Starting cycles"
       daemonize! if @options[:daemonize]
       @run_cycles = true
       while @run_cycles do
@@ -50,7 +54,9 @@ module Blessing
     # Stop running cycles
     def stop
       @run_cycles = false
+      logger.debug "Stopping all runners..."
       stop_runners @config_files
+      logger.info "All runners stopped"
     end
 
     # Main cycle
@@ -58,6 +64,7 @@ module Blessing
     # - start/stop runners
     # - verify/reload runners
     def run_cycle
+      logger.debug "Next cycle"
       refresh_file_list
       start_stop_runners
       reload_runners
@@ -65,12 +72,14 @@ module Blessing
 
     # Refresh config file list and preserve old list
     def refresh_file_list
+      logger.debug "Refreshing file list"
       # Preserve old file list
       @old_config_files = @config_files
 
       files = []
       @patterns.each{|p| files += Dir.glob(p)}
       @config_files = files.uniq.sort
+      logger.debug "Found files: #{@config_files.inspect}"
     end
 
     # Find differences in old and new config file lists
@@ -83,6 +92,7 @@ module Blessing
 
     # Start runners for added config files
     def start_runners(files)
+      logger.debug "Starting runners for: #{files.inspect}"
       files.each { |conf|
         @runners[conf] = runner = Blessing::Runner.new(conf)
         runner.start
@@ -91,6 +101,7 @@ module Blessing
 
     # Stop runners for missing config files
     def stop_runners(files)
+      logger.debug "Stopping runners for: #{files.inspect}"
       files.each { |conf| 
         if @runners[conf]
           @runners[conf].stop
